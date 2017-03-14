de8ee280c6c436ef5f62d2408c79276b25ae3eeb
rohitpoduri12 <np2581@columbia.edu>
2017-03-14 17:00:50 -0400

    Changed Web UI

--- application.py
+++ application.py
@@ -1,12 +1,13 @@
 
 from flask import Flask, render_template, request
 from elasticsearch import Elasticsearch
-#import tweepy
+#import thread
 import certifi
+#import file StreamData
 
 application = Flask(__name__)
 
-keyword_dict = {'keyword 1':'movies','keyword 2':'technology','keyword 3':'sports','keyword 4':'life','keyword 5':'news','keyword 6':'travel','keyword 7':'health','keyword 8':'awesome','keyword 9':'energy','keyword 10':'music','Nothing here':'','':''}
+keyword_dict = {'keyword 1':'movies','keyword 2':'technology','keyword 3':'sports','keyword 4':'life','keyword 5':'news','keyword 6':'travel','keyword 7':'health','keyword 8':'awesome','keyword 9':'energy','keyword 10':'music','Nothing here':'nothing','':'nothing'}
 geo_dict = {'d1':10,'d2':50,'d3':100,'d4':200,'d5':500,'d6':1000,'d7':5000,'Nothing here':0,'': 0}
 es = Elasticsearch(hosts = [{"host" : "search-tweetmap-utivo42pdpwisoy7ttbejzf7z4.us-east-1.es.amazonaws.com",
                               "port" : 443}],
@@ -24,6 +25,11 @@ def update_map():
         key = request.form['keyword']
         dist = request.form['geo_dist']
         tweets = gettweets(keyword_dict[key])
+      
+        
+    #if request.method == 'GET':
+        
+    #    tweets = [{'lng': 1, 'id': 8, 'tweet': 'A', 'lat': 5}]
             
     return render_template("index.html",lat = lat, long = long, key = keyword_dict[key], dist = geo_dict[dist], tweets = tweets)
     
@@ -32,6 +38,9 @@ def gettweets(keyword):
     
     tweets = []
     
+    if keyword == "nothing":
+        keyword = ""
+
     #body2 = {"query":{"match_all":{}}}
     body2 = {"query":{"match":{"_all":keyword}}}
 
@@ -45,14 +54,14 @@ def getgeotweets(latitude, longitude, distance):
     
     distance_string = distance + 'km'
     
-    body = {"query": {"match_all": {}}, "filter": {"geo_distance": { "distance": distance_string, "distance_type": "sloppy_arc", "location": { "lat": latitude, "lon": longitude 	}}}}
+    body1 = {"query": {"match_all": {}}, "filter": {"geo_distance": { "distance": distance_string, "distance_type": "sloppy_arc", "location": { "lat": latitude, "lon": longitude 	}}}}
 
-    size = 10000
-    result = es.search(index = "final-tweet-index", doc_type = "twitter", body, size)
+    result = es.search(index = "final-tweet-index", doc_type = "twitter", body = body1, size = 10000)
 
     return result
     
     
 
 if __name__ == "__main__":
+    #thread.start_new_thread(startTwitterRequests, ())
     application.run(host='0.0.0.0', debug = True)

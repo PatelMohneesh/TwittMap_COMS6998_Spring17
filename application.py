5967e9d67737354ff2eb4e97b255f2277c9638a0
rohitpoduri12 <np2581@columbia.edu>
2017-03-14 04:11:05 -0400

    Added geospatial code, not yet deployed

--- application.py
+++ application.py
@@ -6,28 +6,26 @@ import certifi
 
 application = Flask(__name__)
 
-keyword_dict = {'keyword 1':'movies','keyword 2':'technology','keyword 3':'sports','keyword 4':'soccer','keyword 5':'news','keyword 6':'engineering','keyword 7':'twitter','keyword 8':'google','keyword 9':'facebook','keyword 10':'amazon','Nothing here':'','':''}
-
-es = Elasticsearch(hosts = [{"host" : "search-mp3542-1-posiat2uo7jopakbg7bacyvcbi.us-east-1.es.amazonaws.com",
+keyword_dict = {'keyword 1':'movies','keyword 2':'technology','keyword 3':'sports','keyword 4':'life','keyword 5':'news','keyword 6':'travel','keyword 7':'health','keyword 8':'awesome','keyword 9':'energy','keyword 10':'music','Nothing here':'','':''}
+geo_dict = {'d1':10,'d2':50,'d3':100,'d4':200,'d5':500,'d6':1000,'d7':5000,'Nothing here':0,'': 0}
+es = Elasticsearch(hosts = [{"host" : "search-tweetmap-utivo42pdpwisoy7ttbejzf7z4.us-east-1.es.amazonaws.com",
                               "port" : 443}],
                               use_ssl='True')
 
 @application.route ('/',methods = ['POST', 'GET'])
 def update_map():
-    lat = 0
-    long = 0
+    lat = 0.00
+    long = 0.00
     key = "Nothing here"
-    #tweets = [{'_type': 'twitter', '_source': {'content': "#top3apps for 'cm okram ibobi singh'\n\ntwitter for android 45%\ntwitter web client 17%\ntwitter for iphone 10%", 'user': 'Trendinalia India', 'user_id': 1270150182, 'coordinates': [40.7128, -74.0059]}, '_id': 'AVrHfR1W0V-fCyN2tAXe', '_index': 'cloud_tweet', '_score': 4.2272134},
-    #                                           {'_type': 'twitter', '_source': {'content': "#top3apps for 'cm okram ibobi singh'\n\ntwitter for android 45%\ntwitter web client 17%\ntwitter for iphone 10%", 'user': 'Trendinalia India', 'user_id': 1270150182, 'coordinates': [49.7128, -70.0059]}, '_id': 'AVrHfR1W0V-fCyN2tAXe', '_index': 'cloud_tweet', '_score': 4.2272134},
-    #                                                                            {'_type': 'twitter', '_source': {'content': "#top3apps for 'cm okram ibobi singh'\n\ntwitter for android 45%\ntwitter web client 17%\ntwitter for iphone 10%", 'user': 'Trendinalia India', 'user_id': 1270150182, 'coordinates': [52.7128, -73.0059]}, '_id': 'AVrHfR1W0V-fCyN2tAXe', '_index': 'cloud_tweet', '_score': 4.2272134}]
-    #tweets = ["dddsd","dsdsd","dsdds"]
+    dist = "Nothing here"
     tweets = []
     if request.method == 'POST':
         
         key = request.form['keyword']
+        dist = request.form['geo_dist']
         tweets = gettweets(keyword_dict[key])
             
-    return render_template("index.html",lat = lat, long = long, key = keyword_dict[key], tweets = tweets)
+    return render_template("index.html",lat = lat, long = long, key = keyword_dict[key], dist = geo_dict[dist], tweets = tweets)
     
     
 def gettweets(keyword):
@@ -37,11 +35,24 @@ def gettweets(keyword):
     #body2 = {"query":{"match_all":{}}}
     body2 = {"query":{"match":{"_all":keyword}}}
 
-    stream = es.search(index = "cloud_tweet", doc_type = "twitter", body = body2, size = 10000)
+    stream = es.search(index = "final-tweet-index", doc_type = "twitter", body = body2, size = 10000)
     
     tweets = stream["hits"]["hits"]
     
     return tweets
+    
+def getgeotweets(latitude, longitude, distance):
+    
+    distance_string = distance + 'km'
+    
+    body = {"query": {"match_all": {}}, "filter": {"geo_distance": { "distance": distance_string, "distance_type": "sloppy_arc", "location": { "lat": latitude, "lon": longitude 	}}}}
+
+    size = 10000
+    result = es.search(index = "final-tweet-index", doc_type = "twitter", body, size)
+
+    return result
+    
+    
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', debug = True)

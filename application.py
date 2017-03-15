56cc3bcefffe70b415d28c3e69d9721239de84c2
rohitpoduri12 <np2581@columbia.edu>
2017-03-14 21:19:27 -0400

    Final Submitted version

--- application.py
+++ application.py
@@ -5,8 +5,11 @@ import certifi
 
 application = Flask(__name__)
 
+#Keywords
 keyword_dict = {'keyword 1':'movies','keyword 2':'technology','keyword 3':'sports','keyword 4':'life','keyword 5':'news','keyword 6':'travel','keyword 7':'health','keyword 8':'awesome','keyword 9':'energy','keyword 10':'music','Nothing here':'no','':'no'}
 geo_dict = {'d1':10,'d2':50,'d3':100,'d4':200,'d5':500,'d6':1000,'d7':5000,'Nothing here':0,'': 0}
+
+#Define ElasticSearch credentials
 es = Elasticsearch(hosts = [{"host" : "search-tweetmap-utivo42pdpwisoy7ttbejzf7z4.us-east-1.es.amazonaws.com",
                               "port" : 443}],
                               use_ssl='True')
@@ -23,7 +26,8 @@ def update_map():
         key = request.form['keyword']
         dist = request.form['geo_dist']
         tweets = gettweets(keyword_dict[key])
-            
+    
+    #Update HTML webpage
     return render_template("index.html",lat = center_lat, long = center_long, key = keyword_dict[key], dist = geo_dict[dist], tweets = tweets)
 
 def gettweets(keyword):
@@ -34,6 +38,8 @@ def gettweets(keyword):
         keyword = ""
 
     body_content = {"query":{"match":{"_all":keyword}}}
+
+    #Get relevant tweet stream
     stream = es.search(index = "final-tweet-index", doc_type = "twitter", body = body_content, size = 10000)    
     tweets = stream["hits"]["hits"]
     
